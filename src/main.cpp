afa73133c6f8d1abae5786c0d169e418c9767af9
joe wezorek <jwezorek@gmail.com>
2026-06-11 10:43:29 -0700

    catch exceptions in the main function

--- src/main.cpp
+++ src/main.cpp
@@ -13,48 +13,56 @@ int main(int argc, char* argv[]) {
         std::println(std::cerr, "usage: {} <puzzle.txt> [command]", argv[0]);
         return 1;
     }
+    try {
+        auto cmd_line = args_to_strings(argc, argv);
 
-    auto cmd_line = args_to_strings(argc, argv);
-
-    std::string inp_path = cmd_line.front();
-    if (!fs::exists(inp_path)) {
-        std::println(std::cerr, "error: file does not exist: {}", inp_path);
-        return 1;
-    }
-
-    auto color_puzzle = load_game_state(inp_path);
-    if (cmd_line.size() == 1) {
-        
-        auto solution = solve(color_puzzle);
-        std::println("best solution is {} moves:", solution.size());
-        for (const auto& move : solution) {
-            std::println("{}", move_to_string(move));
+        std::string inp_path = cmd_line.front();
+        if (!fs::exists(inp_path)) {
+            std::println(std::cerr, "error: file does not exist: {}", inp_path);
+            return 1;
         }
-        return 0;
-    }
 
-    auto arg = cmd_line.back();
-    if (arg == "catalog") {
-        auto catalog = catalog_solutions(color_puzzle);
-        for (const auto& solution : catalog) {
-            std::println("{} is solvable in {} moves.", solution.opening, solution.num_moves);
-        }
-    } else {
-        auto color_1 = color_from_char(arg.front());
-        auto color_2 = color_from_char(arg.back());
-        if (!color_1 || !color_2) {
-            std::println(std::cerr, "error: invalid opening colors");
-            return -1;
-        }
-        auto solution = solve(color_puzzle, *color_1, *color_2);
-        if (!solution.empty()) {
-            std::println("Solution is {} moves:", solution.size());
+        auto color_puzzle = load_game_state(inp_path);
+        if (cmd_line.size() == 1) {
+
+            auto solution = solve(color_puzzle);
+            std::println("best solution is {} moves:", solution.size());
             for (const auto& move : solution) {
                 std::println("{}", move_to_string(move));
             }
-        } else {
-            std::println("no solution with {}", arg);
+            return 0;
         }
+
+        auto arg = cmd_line.back();
+        if (arg == "catalog") {
+            auto catalog = catalog_solutions(color_puzzle);
+            for (const auto& solution : catalog) {
+                std::println("{} moves with {}", solution.num_moves, solution.opening);
+            }
+        } else {
+            auto color_1 = color_from_char(arg.front());
+            auto color_2 = color_from_char(arg.back());
+            if (!color_1 || !color_2) {
+                std::println(std::cerr, "error: invalid opening colors");
+                return -1;
+            }
+            auto solution = solve(color_puzzle, *color_1, *color_2);
+            if (!solution.empty()) {
+                std::println("Solution is {} moves:", solution.size());
+                for (const auto& move : solution) {
+                    std::println("{}", move_to_string(move));
+                }
+            }
+            else {
+                std::println("no solution with {}", arg);
+            }
+        }
+    } catch (std::runtime_error e) {
+        std::println("{}", e.what());
+        return -1;
+    } catch (...) {
+        std::println("unknown error");
+        return -1;
     }
 
     return 0;
